12bfea01be2d50d5caf9ca2df5aea3a78a4fe292
Tsukasa <admin@tsukasaze.com>
2025-10-02 12:54:16 +0900

    Add files via upload

--- src/bridge/bridge_api.h
+++ src/bridge/bridge_api.h
@@ -1,17 +1,22 @@
-// src/bridge/bridge_api.h
 #pragma once
 #include <stdint.h>
 
-#ifdef _WIN32
-  #define SC2_API extern "C" __declspec(dllexport)
+#if defined(_WIN32)
+  #define MS_API extern "C" __declspec(dllexport)
 #else
-  #define SC2_API extern "C" __attribute__((visibility("default")))
+  #define MS_API extern "C" __attribute__((visibility("default")))
 #endif
 
 struct CSingleWorldRep;
 
-SC2_API void SC2_Init(const char* game_dir);
-SC2_API void SC2_OnLevelInit(const char* map_name, const char* game_dir);
-SC2_API void SC2_OnLevelShutdown();
-SC2_API void SC2_OnWorldCreated(CSingleWorldRep* singleWorld);
+MS_API void SC2_Init(const char* game_dir);
+MS_API void SC2_OnLevelInit(const char* map_name, const char* game_dir);
+MS_API void SC2_OnLevelShutdown();
+MS_API void SC2_OnWorldCreated(CSingleWorldRep* singleWorld);
 
+// Minimal bridge API expected by InterfaceBridge.cs
+MS_API int  ms_bridge_init();
+MS_API void ms_bridge_shutdown();
+MS_API int  ms_bridge_load_gamedata(const char* path_utf8);
+MS_API int  ms_bridge_hook_by_name(const char* key_utf8, void* detour_fn_ptr, void** out_handle);
+MS_API void ms_bridge_unhook(void* handle);
